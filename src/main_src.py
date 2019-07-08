f97f626bec02f06ec2050b26948f2193f4a15015
Samir <abdallahsamir99@gmail.com>
2019-07-08 09:39:06 +0200

    some edits for better performance

--- src/main_src.py
+++ src/main_src.py
@@ -47,9 +47,11 @@ if os.path.exists(out_directory):
 
 dep_id_list = []
 #read p.csv file
-p_csv_file = open(p_directory, newline='')
-p_reader = csv.DictReader(p_csv_file)
+p_csv_file  = open(p_directory, newline='')
+p_reader    = csv.DictReader(p_csv_file)
+p_list      = []
 for row in p_reader:
+    p_list.append(row)
     #get list of dep_id alng with tot
     dep_id = int(row[DEP_ID])
     if [dep_id, 1] not in dep_id_list:
@@ -69,24 +71,36 @@ writer.writeheader()
 #order dep_id_list
 dep_id_list = sort_list(dep_id_list)
 
+flag        = 1
+o_list      = []
+out_list    = []
 for dep_id in dep_id_list:
     #for each dep_id search p.csv for p_id
     num_first = 0
-    p_csv_file = open(p_directory, newline='')
-    p_reader = csv.DictReader(p_csv_file)
-    for p_row in p_reader:
+    for p_row in p_list:
         if int(p_row[DEP_ID]) == dep_id[0]:
             p_id = int(p_row[P_ID])
             #for each p_id search o.csv for reordered values
-            o_csv_file = open(o_directory, newline='')
-            o_reader = csv.DictReader(o_csv_file)
-            for o_row in o_reader:
-                if int(o_row[P_ID]) == p_id:
-                    reordered = int(o_row[REORDERED])
-                    if reordered == 0:
-                        #set num_first to 1 if reordered = 0
-                        num_first = 1
-                        break
+            if flag == 0:
+                o_csv_file = open(o_directory, newline='')
+                o_reader = csv.DictReader(o_csv_file)
+                for o_row in o_reader:
+                    o_list.append(o_row)
+                    if int(o_row[P_ID]) == p_id:
+                        reordered = int(o_row[REORDERED])
+                        if reordered == 0:
+                            #set num_first to 1 if reordered = 0
+                            num_first = 1
+                            break
+            else:
+                for o_row in o_list:
+                    if int(o_row[P_ID]) == p_id:
+                        reordered = int(o_row[REORDERED])
+                        if reordered == 0:
+                            #set num_first to 1 if reordered = 0
+                            num_first = 1
+                            break
+
     
     record = {
         DEP_ID      : dep_id[0],
@@ -94,16 +108,14 @@ for dep_id in dep_id_list:
         NUM_FIRST   : num_first,
         PERCENT     : round(num_first/dep_id[1], 2)
     }
+    out_list.append(record)
     #write the calculated values in out.csv
     writer.writerow(record)
 
 out_csv_file.close()
 
-out_csv_file = open(out_directory, newline='')
-out_reader = csv.DictReader(out_csv_file)
-
 #print field names
 print('dep_id' + '\t\t' +'tot' + '\t\t' +'num_first' + '\t' + 'percent')
 #print values
-for row in out_reader:
-    print(row[DEP_ID] + '\t\t' + row[TOT] + '\t\t' + row[NUM_FIRST] + '\t\t' + row[PERCENT])
+for row in out_list:
+    print(str(row[DEP_ID]) + '\t\t' + str(row[TOT]) + '\t\t' + str(row[NUM_FIRST]) + '\t\t' + str(row[PERCENT]))
